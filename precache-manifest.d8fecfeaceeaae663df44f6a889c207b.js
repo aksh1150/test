436a75591ff68fb7a08c715ac9ccd1bc2589a646
Akshit <akshit@Akshits-iMac.local>
2020-02-22 21:57:07 -0500

    Updates

---
+++ precache-manifest.d8fecfeaceeaae663df44f6a889c207b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8301cfae798daddd4b06a0025d9db4f",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8bbe7e05693071c629e1",
+    "url": "/test/static/css/main.de4d394e.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/test/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbe7e05693071c629e1",
+    "url": "/test/static/js/main.212a3aad.chunk.js"
+  },
+  {
+    "revision": "675e89a1f0ac737fd0cc",
+    "url": "/test/static/js/runtime-main.e608a069.js"
+  }
+]);
